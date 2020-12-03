d7c2b5921336beb511ab3ecdb99b786ff2c50788
Justin Cardinal <justin@somuchuptime.com>
2020-12-03 11:28:24 -0600

    Solve Day 2 Puzzle 1

---
+++ 02-password-philosophy/part1.py
@@ -0,0 +1,21 @@
+with open('input.txt', 'r') as f:
+    password_list = f.read().splitlines()
+
+valid_password_count = 0
+
+for item in password_list:
+    min = int(item[:int(item.index('-'))])
+    max = int(item[int(item.index('-')) + 1:item.index(' ')])
+    test_char = item[int(item.index(':') - 1)]
+    test_pass = item[int(item.index(':') + 2):]
+    char_count = 0
+    
+    for char in test_pass:
+        if char == test_char:
+            char_count += 1
+    
+    if char_count >= min and char_count <= max:
+        valid_password_count += 1
+
+
+print(valid_password_count)
